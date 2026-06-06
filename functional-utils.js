793dd6861bfb1f606f57687b54b9a6fb978af62a
Gefferson Casasola <gefferson.casasola@outlook.com>
2026-06-06 18:15:06 -0500

    feat: implementar búsqueda de gastos y cálculo de saldo

--- functional-utils.js
+++ functional-utils.js
@@ -3,3 +3,9 @@ const obtenerIngresos = (valores) => valores.filter((valor) => valor > 0);
 const obtenerGastos = (valores) => valores.filter((valor) => valor < 0);
 
 const montosAbsolutos = (valores) => valores.map((valor) => Math.abs(valor));
+
+const buscarPrimerGastoMayor = (valores, monto) =>
+  valores.find((valor) => valor < -monto);
+
+const calcularSaldo = (valores) =>
+  valores.reduce((acumulador, valor) => acumulador + valor, 0);
